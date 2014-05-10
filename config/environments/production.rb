17dfa05ff76d913e291bbcaea9daf1dbb9d30622
khcr <kocher.ke@gmail.com>
2014-05-10 13:17:26 +0200

    compile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Eebulle::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( table.js )
+  config.assets.precompile += %w( table.js mail.css.scss )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
