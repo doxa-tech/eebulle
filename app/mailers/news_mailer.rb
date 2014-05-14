17701bfb407f8ffb7aa7e65f5b09640d8aee0a3d
Noémien Kocher <nkcr.je@gmail.com>
2014-05-14 10:41:21 +0200

    mailer comments

--- app/mailers/news_mailer.rb
+++ app/mailers/news_mailer.rb
@@ -1,5 +1,8 @@
 class NewsMailer < ActionMailer::Base
   default from: "\"David de l'EEBulle\" <david.hausmann@eebulle.ch>"
+  #
+  # To overwrite, absolutely need to reset them to nil !
+  #
   default 'X-MC-AutoText' => 1
 	default 'X-MC-InlineCSS'=> "true"
 
