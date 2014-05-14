1baf58eba5e38862bc2a7321d28bcd91230a7064
Noémien Kocher <nkcr.je@gmail.com>
2014-05-14 10:36:32 +0200

    remove track opens from header mails

--- app/mailers/news_mailer.rb
+++ app/mailers/news_mailer.rb
@@ -2,7 +2,6 @@ class NewsMailer < ActionMailer::Base
   default from: "\"David de l'EEBulle\" <david.hausmann@eebulle.ch>"
   default 'X-MC-AutoText' => 1
 	default 'X-MC-InlineCSS'=> "true"
-  default 'X-MC-Track' => "clicks_all"
 
   def news(content, subject)
   	@content = content
