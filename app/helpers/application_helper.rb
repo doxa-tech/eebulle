b5de91f7e841cb9a492f8d1ae43244fd62b2d2cc
khcr <kocher.ke@gmail.com>
2015-04-15 21:57:09 +0200

    useless code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,5 @@
 module ApplicationHelper
 
-	def present(table)
-		render 'table', presenter: table
-	end
-
 	def authorized?(level = 4)
     unless current_user && current_user.level <= level
       return false
