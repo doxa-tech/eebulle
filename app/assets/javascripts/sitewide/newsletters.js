9451254100bf4ec7817ee00d8aaf98d2817bca0e
khcr <kocher.ke@gmail.com>
2015-07-06 13:44:16 +0200

    add view button

---
+++ app/assets/javascripts/sitewide/newsletters.js
@@ -0,0 +1,22 @@
+$(document).on("ready page:load", function() {
+
+  var newslettersTable = $("#newsletters-table"),
+      viewButton = newslettersTable.find(".view");
+
+  // line clickable
+  newslettersTable.on("click", "tbody tr", function(e) {
+    var id = $(this).data('url') ;
+    if ( typeof id !== 'undefined' ) {
+      viewButton.addClass("on");
+      viewButton.attr('href', "/newsletters/" + id);
+    }
+  });
+
+  // Double click
+  newslettersTable.on("dblclick", "tbody tr", function() {
+    var id = $(this).data('url');
+    if ( typeof id !== 'undefined' ) {
+      window.location = "/newsletters/" + id;
+    }
+  });
+});
