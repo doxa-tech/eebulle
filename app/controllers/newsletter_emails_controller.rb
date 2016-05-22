6b41c2c406730a391b4be08766e7ffb8cea0b8e2
Noémien Kocher <nkcr.je@gmail.com>
2016-05-22 20:22:01 +0200

    email controller refactoring

--- app/controllers/newsletter_emails_controller.rb
+++ app/controllers/newsletter_emails_controller.rb
@@ -5,7 +5,12 @@ class NewsletterEmailsController < ApplicationController
 	end
 
 	def create
-		if NewsletterEmail.exists?(newsletter_email_params)
+		@newsletter_email = NewsletterEmail.new(newsletter_email_params)
+		if @newsletter_email.save
+			NewsMailer.confirmation(newsletter_email_params[:email]).deliver_now
+			redirect_to root_path, success: t('newsletter_email.new.success')
+		elsif @newsletter_email.errors.added? :email, :taken
+			p @newsletter_email.to_yaml
 			if NewsletterEmail.where(newsletter_email_params).first.confirmed
 				redirect_to root_path, notice: t('newsletter_email.new.exists')
 			else
@@ -13,19 +18,17 @@ class NewsletterEmailsController < ApplicationController
 				redirect_to root_path, notice: t('newsletter_email.new.resend')
 			end
 		else
-			@newsletter_email = NewsletterEmail.new(newsletter_email_params)
-			if @newsletter_email.save
-				NewsMailer.confirmation(newsletter_email_params[:email]).deliver_now
-				redirect_to root_path, success: t('newsletter_email.new.success')
-			else
-				render 'new'
-			end
+			render 'new'
 		end
 	end
 
 	def confirmation
 		newsletter_email = Subscriber.read_confirmation_token(params[:signature])
-		if !newsletter_email.nil? && newsletter_email.update_attributes(confirmed: true)
+		if newsletter_email.nil?
+			redirect_to root_path, error: t('newsletter_email.confirmation.error')
+		elsif newsletter_email.confirmed
+			redirect_to root_path, error: t('newsletter_email.confirmation.already')
+		elsif newsletter_email.update_attributes(confirmed: true)
 			# add to mailgun email list
 			MailgunList.add(newsletter_email.email)
 		else
