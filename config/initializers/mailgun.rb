fdbe28206984e9be36aec2883dcb6f0d1b1e9c0a
Noémien Kocher <nkcr.je@gmail.com>
2016-05-23 10:46:40 +0200

    update right mailgun domain

--- config/initializers/mailgun.rb
+++ config/initializers/mailgun.rb
@@ -1,6 +1,6 @@
 # Initialize your Mailgun object:
 Mailgun.configure do |config|
   config.api_key = Rails.application.secrets.mailgun_api_key
-  config.domain  = 'eebulle.ch'
+  config.domain  = 'mailgun.eebulle.ch'
 end
 ::MailgunList = Mailgun().list_members(Rails.application.secrets.mailgun_mailing_list)
