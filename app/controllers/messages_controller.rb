74d6bfadd357b5fb44ff08a14f26eb784aa99be3
Noémien Kocher <nkcr.je@gmail.com>
2017-11-09 11:07:05 +0100

    Update messages_controller.rb
    display all teaching messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,8 +3,8 @@ class MessagesController < ApplicationController
 
 	def index
 		@preachings = Message.preaching.where('date >= ?', 1.month.ago).order('date DESC')
-    @homegroups = Message.homegroup.where('date >= ?', 1.month.ago).order('date DESC')
-    @teachings  = Message.teaching.where('date >= ?', 1.month.ago).order('date DESC')
+    		@homegroups = Message.homegroup.where('date >= ?', 1.month.ago).order('date DESC')
+    		@teachings  = Message.teaching.all
 	end
 
 end
