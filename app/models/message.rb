7deb0f54647c227b781844d85ab20be45bee8101
Noémien Kocher <nkcr.je@gmail.com>
2017-12-07 11:22:58 +0100

    Some updates
    - added message category
    - Updated text on message
    - Added Impact logo

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ApplicationRecord
-  enum category: [:preaching, :homegroup, :teaching]
+  enum category: [:preaching, :homegroup, :teaching, :bible_study]
 
 	validates :name, presence: true, length: { maximum: 80 }
 	validates :speaker, presence: true, length: { maximum: 80 }
