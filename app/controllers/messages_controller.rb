206896b7eb970eeadefe895e94f9adea24c5878d
Noémien Kocher <nkcr.je@gmail.com>
2018-03-22 12:15:26 +0100

    added VIP category for Message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,10 +2,11 @@ class MessagesController < ApplicationController
 	before_action { |c| c.authenticate(:messages) }
 
 	def index
-		@preachings = Message.preaching.where('date >= ?', 1.month.ago).order('date DESC')
-		@homegroups = Message.homegroup.where('date >= ?', 1.month.ago).order('date DESC')
-		@teachings  = Message.teaching.all.order(:name)
-		@bible_studies = Message.bible_study.all.order(:name)
+		@vips = Message.vip.order(name: :asc)
+		@preachings = Message.preaching.where('date >= ?', 1.month.ago).order(date: :desc)
+		@homegroups = Message.homegroup.where('date >= ?', 1.month.ago).order(date: :desc)
+		@teachings  = Message.teaching.order(name: :asc)
+		@bible_studies = Message.bible_study.order(name: :asc)
 	end
 
 end
