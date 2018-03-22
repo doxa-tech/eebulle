de76c1ff1b972c3193ae5286f09c1d6bbe382d4c
Noémien Kocher <nkcr.je@gmail.com>
2018-03-22 12:15:26 +0100

    added VIP category for Message

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ApplicationRecord
-  enum category: [:preaching, :homegroup, :teaching, :bible_study]
+  enum category: [:preaching, :homegroup, :teaching, :bible_study, :vip]
 
 	validates :name, presence: true, length: { maximum: 80 }
 	validates :speaker, presence: true, length: { maximum: 80 }
