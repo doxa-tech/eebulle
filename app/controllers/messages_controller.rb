37bebc4c72fec25bc0823351c5d538806a4e899b
Noémien Kocher <nkcr.je@gmail.com>
2018-04-26 11:39:10 +0200

    updated bible_study order

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,7 +6,7 @@ class MessagesController < ApplicationController
 		@preachings = Message.preaching.where('date >= ?', 1.month.ago).order(date: :desc)
 		@homegroups = Message.homegroup.where('date >= ?', 1.month.ago).order(date: :desc)
 		@teachings  = Message.teaching.order(name: :asc)
-		@bible_studies = Message.bible_study.order(name: :asc)
+		@bible_studies = Message.bible_study.sort_by { |msg| msg.name.gsub(/[^0-9]/, '').to_i }
 	end
 
 end
