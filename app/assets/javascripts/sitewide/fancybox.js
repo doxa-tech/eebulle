c15b5c8d8233548388d3a6ee3743911feb876733
Keran Kocher <kocher.ke@gmail.com>
2018-09-06 19:41:25 +0200

    Revert "fix javascript highlights page"
    This reverts commit 0a08caf470519a13741b7e9e3e8c5a3c033ad395.

--- app/assets/javascripts/sitewide/fancybox.js
+++ app/assets/javascripts/sitewide/fancybox.js
@@ -1,15 +1,12 @@
 $(document).on("turbolinks:load", function() {
-  var fancyButton = $(".fancybox-button");
-  if(fancyButton.length) {
-    fancyButton.fancybox({
-      prevEffect		: 'none',
-      nextEffect		: 'none',
-      closeBtn		: false,
-      parent : "body",
-      helpers		: {
-        title	: { type : 'inside' },
-        buttons	: {}
-      }
-    });
-  }
+  $(".fancybox-button").fancybox({
+    prevEffect		: 'none',
+    nextEffect		: 'none',
+    closeBtn		: false,
+    parent : "body",
+    helpers		: {
+      title	: { type : 'inside' },
+      buttons	: {}
+    }
+  });
 });
